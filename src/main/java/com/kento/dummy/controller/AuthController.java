20662d2b120bacc55fad420aabaf8ae26d0a6ca6
hamadaknd <hamadaknd@pm.nttdata.co.jp>
2018-10-17 20:17:50 +0900

    アクセストークン対応

--- src/main/java/com/kento/dummy/controller/AuthController.java
+++ src/main/java/com/kento/dummy/controller/AuthController.java
@@ -2,8 +2,11 @@ package com.kento.dummy.controller;
 
 import com.kento.dummy.domain.model.AuthInfo;
 import com.kento.dummy.domain.model.GoogleTask;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.jaas.LoginExceptionResolver;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,18 +23,27 @@ import java.util.List;
 @RequestMapping
 public class AuthController {
 
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
     @GetMapping("/auth")
     public String login() {
         return "auth/index";
     }
 
     @GetMapping("/auth/result")
-    public String auth(OAuth2AuthenticationToken oAuth2AuthenticationToken, Model model) {
+    public String auth(OAuth2AuthenticationToken oAuth2AuthenticationToken, AuthInfo authInfo, Model model) {
 
-        AuthInfo authInfo = new AuthInfo();
+        OAuth2AuthorizedClient oAuth2AuthorizedClient =
+                oAuth2AuthorizedClientService.loadAuthorizedClient(
+                        oAuth2AuthenticationToken.getAuthorizedClientRegistrationId(),
+                        oAuth2AuthenticationToken.getName());
+
+        String accessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
 
         authInfo.setName((String) oAuth2AuthenticationToken.getPrincipal().getAttributes().get("name"));
         authInfo.setEmailAddress((String) oAuth2AuthenticationToken.getPrincipal().getAttributes().get("email"));
+        authInfo.setAccessToken(accessToken);
 
         model.addAttribute("authInfo", authInfo);
 
